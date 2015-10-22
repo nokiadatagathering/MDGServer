1286e95baa5fea8a89efe04c8947878b663f3de4
Aleksandra Zolokotskaya <zolokot@wookieelabs.com>
2015-10-22 13:29:09 +0300

    fix bugs with auth in Edge. change select to  dropdown. fix tab-navigation on Edge (archive, upload XML btns)

--- src/components/authorization/authorization.service.js
+++ src/components/authorization/authorization.service.js
@@ -7,9 +7,10 @@
     var
       LANGUAGES_URL = '/supportedLanguages',
       GET_USER_PERMISSION_URL = '/userPermission',
-      LOGOUT_URL = '/logout';
+      LOGOUT_URL = '/logout',
+      cacheBuster = new Date().getTime();
 
-    function getLanguages () {
+      function getLanguages () {
       return $http.get(LANGUAGES_URL);
     }
 
@@ -18,8 +19,9 @@
     }
 
     function getUserPermission () {
-      return $http.get(GET_USER_PERMISSION_URL)
+      return $http.get(GET_USER_PERMISSION_URL + '?t=' + cacheBuster)
         .success(function (result) {
+          console.log('result', result);
         });
     }
 
