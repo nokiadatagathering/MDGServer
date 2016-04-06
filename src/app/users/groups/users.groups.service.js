5962415fe8517ea7938e0162e1c22fafd7414da2
Anna Vernidub <anna.vernidub13@gmail.com>
2016-04-06 13:53:22 +0300

    fix bug with adding user to group

--- src/app/users/groups/users.groups.service.js
+++ src/app/users/groups/users.groups.service.js
@@ -9,32 +9,31 @@
 
       return $http.get(GROUPS_URL + '?cacheBuster=' +  new Date().getTime())
         .success(function (result) {
-          console.log('result', result);
         });
     }
 
     function groupData(groupId) {
-      var group_url = GROUPS_URL +'/' + groupId;
+      var group_url = GROUPS_URL + '/' + groupId;
       return $http.get(group_url)
         .success(function (result) {
         });
     }
 
     function createGroup(groupName) {
-      return $http.post(GROUPS_URL, {name: groupName})
+      return $http.post(GROUPS_URL, { name: groupName })
         .success(function (result) {
         });
     }
 
     function updateGroup(groupId, groupData) {
-      var group_url = GROUPS_URL + groupId;
+      var group_url = GROUPS_URL + '/' + groupId;
       return $http.put(group_url, groupData)
         .success(function (result) {
         });
     }
 
     function deleteGroup(groupId) {
-      var delete_group_url = GROUPS_URL +'/' + groupId;
+      var delete_group_url = GROUPS_URL + '/' + groupId;
       return $http.delete(delete_group_url)
         .success(function (result) {
         });
